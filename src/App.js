0b0627272c7b46b0d89b5378d428f1b5c8e0ab3d
Rick Hood <richard.hood@commonmediainc.com>
2019-01-07 19:31:01 -0500

    misc work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './lil-nerdy.png';
 import './App.css';
 
 class App extends Component {
+
+  // this is where the App state is stored
+  state = {
+    spin: true,
+    nerdy: "spin"
+  }
+
+  // this is called an arrow function
+  setspin = () => {
+    this.setState({
+    spin: !this.state.spin,
+    })
+    if(this.state.nerdy == "spin"){
+      this.setState({
+        nerdy: "no-spin",
+      })
+    } else {
+      this.setState({
+        nerdy: "spin",
+      })
+    }
+  }
+
+  // remder is a required part of a component, this is JSX (html within Javascript)
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <header className="App-header">
+          <Nerdy spinstate = {this.state.spin} nerdyclass = {this.state.nerdy}  />
+          <button onClick={this.setspin}>Spin Control</button>
         </header>
+      </div>
+    );
+  }
+}
+
+// this is a new component called Nerdy
+class Nerdy extends Component {
+  render() {
+    return (
+      <div className="Nerdy">
+      <img src={logo} id="App-logo" className={this.props.nerdyclass} alt="logo" />
+      { this.props.spinstate &&
+      <p>Lil' Nerdy is spinning.</p>
+      }
+      { !this.props.spinstate &&
+        <p>Lil' Nerdy no spin.</p>
+      }
       </div>
     );
   }
