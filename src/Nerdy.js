10b79e92912cc566fa8fd247533b9ccbdf2a452f
Rick Hood <richard.hood@commonmediainc.com>
2019-03-08 14:54:34 -0500

    misc

--- src/Nerdy.js
+++ src/Nerdy.js
@@ -3,19 +3,19 @@ import logo from './lil-nerdy.png';
 
 // this is a new component called Nerdy
 class Nerdy extends Component {
-render() {
+  render() {
     return (
-    <div className="Nerdy">
-        <img src={logo} id="App-logo" className={this.props.nerdyclass} alt={this.props.nerdytext} />
-        { this.props.spinstate &&
-        <p>Lil' Nerdy is spinning.</p>
-        }
-        { !this.props.spinstate &&
-            <p>Lil' Nerdy no spin.</p>
-        }
-    </div>
+      <div className="Nerdy">
+          <img src={logo} id="App-logo" className={this.props.nerdyclass} alt={this.props.nerdytext} />
+          { this.props.spinstate &&
+          <p>Lil' Nerdy is spinning.</p>
+          }
+          { !this.props.spinstate &&
+              <p>Lil' Nerdy no spin.</p>
+          }
+      </div>
     );
-    }
+  }
 }
 
 export default Nerdy;
